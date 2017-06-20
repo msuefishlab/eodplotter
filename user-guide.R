deae58cec1b93640e21130b734c34dca02fe3495
Colin <colin.diesh@gmail.com>
2017-06-20 15:43:36 -0400

    Add vignette

---
+++ user-guide.R
@@ -0,0 +1,9 @@
+## ----eval = FALSE--------------------------------------------------------
+#  install.packages('devtools')
+
+## ----eval = FALSE--------------------------------------------------------
+#  devtools::install_github('msuefishlab/eodplotter')
+
+## ----eval = FALSE--------------------------------------------------------
+#  install_github('msuefishlab/eodplotter@0.0.2')
+
